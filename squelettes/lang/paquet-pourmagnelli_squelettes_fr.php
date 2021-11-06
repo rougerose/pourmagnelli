9743e24ca61c5222996eb591177ca0faf90e9112
christophe le drean <christophe@rouge-rose.net>
2021-11-06 22:16:58 +0100

    Ajout d'icones provisoires et des descriptions du plugin

---
+++ squelettes/lang/paquet-pourmagnelli_squelettes_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'pourmagnelli_squelettes_description' => 'Squelettes du site Pour Magnelli',
+	'pourmagnelli_squelettes_slogan' => 'Squelettes du site Pour Magnelli',
+	'pourmagnelli_squelettes_titre' => 'Pour Magnelli : squelettes'
+);
---
+++ theme/lang/paquet-pourmagnelli_theme_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'pourmagnelli_theme_description' => 'Thème graphique du site Pour Magnelli',
+	'pourmagnelli_theme_slogan' => 'Thème graphique du site Pour Magnelli',
+	'pourmagnelli_theme_titre' => 'Pour Magnelli : thème graphique'
+);
