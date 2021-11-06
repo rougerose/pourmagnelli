02932414d80a2ce2b008aa892646b3ed739dafb2
christophe le drean <christophe@rouge-rose.net>
2021-11-06 19:03:07 +0100

    Base theme

---
+++ theme/gulp.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  server: {
+    proxy: "http://localhost:8888/pour-magnelli.dev",
+    notify: false,
+  },
+  tailwind: "tailwind.config.js",
+  scss: {
+    src: "src/scss/"
+  },
+  css: {
+    src: "src/css/",
+    dest: "dist/css/",
+  },
+  js: {
+    src: ["src/js/main.js"],
+    dest: "dist/js/",
+    name: "main.js",
+  },
+  clean: ["dist/**/*", "!dist/"],
+  tasks: {
+    css: true,
+    cssVendor: false,
+    fonts: false,
+    js: false,
+    clean: true,
+    reload: true,
+  },
+};
