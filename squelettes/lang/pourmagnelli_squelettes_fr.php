5e98d6de9e9efc13bf2e4abab3d060e313b732dc
christophe le drean <christophe@rouge-rose.net>
2021-11-16 19:56:35 +0100

    Corrections menus/lien_recherche

--- squelettes/lang/pourmagnelli_squelettes_fr.php
+++ squelettes/lang/pourmagnelli_squelettes_fr.php
@@ -8,7 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// M
 	'menu_lien_recherche_description' => 'Lien vers la page Recherche. Le libellé est remplacé par une icone "recherche"',
 	'menu_lien_recherche_nom' => 'Lien recherche (icone)',
-	'menu_recherche_mobile_entree_css_recherche' => 'Classes CSS du formulaire de recherche [Facultatif]',
+	'menu_recherche_mobile_entree_css_recherche' => 'Classes CSS du formulaire de recherche',
 
 	// N
 	'navigation_fermer' => "Fermer la navigation",
